b28a1a8657247448e536c711918ad91b8687b57a
S. Lindqvist <sanne.lindqvist@protonmail.com>
2026-09-17 15:04:14 +0200

    Finished part F

--- lab6/part_F.py
+++ lab6/part_F.py
@@ -26,6 +26,44 @@ products = [
 in_stock = [product for product in products if product.get("stock") > 0]
 unique_categories = {product.get("category") for product in products}
 
+products_inventory_values = [
+    {
+        product.get("name", ""): product.get("price", 0) *product.get("stock", 0),
+    } 
+    for product in products]
+
+sorted_products_inventory_values = sorted(products_inventory_values, 
+                                          key=lambda product: next(iter(product.values())), 
+                                          reverse=True)
+
+# Two comprehensions for getting all vegetables.
+# The first one is clearer because it has descriptive naming, does not create a list unnecessarily,
+# and it actually checks the category key-value pair precisely. 
+# The second one would return any items that has "Vegetables" anywhere in its values.
+vegetables = [product for product in products if product.get("category") == "Vegetables"]
+vegetables_complicated = [x for x in products if "Vegetables" in list(x.values())]
+
+vegetable_names = [product["name"] for product in products if product.get("category") == "Vegetables"]
+vegetable_stock = [product["stock"] for product in products if product.get("category") == "Vegetables"]
+zipped_vegetables = zip(vegetable_names, vegetable_stock)
+
+
 print(products)
 print(in_stock)
 print(unique_categories)
+print(products_inventory_values)
+print(sorted_products_inventory_values)
+
+for rank, product in enumerate(sorted_products_inventory_values, start=1):
+    name, value = next(iter(product.items()))
+    print(f"{rank}. {name}, {value} kr")
+
+
+for x in test:
+    print(x)
+
+for value in zipped_vegetables:
+    print(value)
+
+print("Vegetables", vegetables)
+print("Vegetables", vegetables_complicated)
