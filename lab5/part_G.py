b8ff1ee0f2efbfac5397a0876d0c7a74249b19c7
S. Lindqvist <sanne.lindqvist@protonmail.com>
2026-09-18 13:49:01 +0200

    Finished part G

--- lab5/part_G.py
+++ lab5/part_G.py
@@ -1,8 +1,17 @@
-def merge_settings(defaults, **overrides):
-    pass
+def merge_settings(defaults: dict, **overrides) -> dict:
+    merged = dict()
+    for key, value in defaults.items():
+        override_value = overrides.get(key)
+        if override_value:
+            merged[key] = override_value
+        else:
+            merged[key] = value
+
+    return merged
 
 def call_summary(function_name: str, *args, **kwargs) -> str:
-    return f"{function_name}({', '.join(args)}, {kwargs})"
+    kwargs_formatted = [f"{item[0]} = {item[1]}" for item in kwargs.items()]
+    return f"{function_name}({', '.join(args)}, {', '.join(kwargs_formatted)})"
 
 def statistics(*numbers):
     count = len(numbers)
@@ -21,9 +30,72 @@ def statistics(*numbers):
     return count, total, average, min, max
 
 
-print(call_summary("test", "arg1", "arg2", "arg3", extra=True))
+default_settings = {
+    "theme": "light",
+    "language": "English",
+    "month": "September"
+}
+
+new_settings = merge_settings(default_settings, theme="dark", language="Swedish")
+print(new_settings)
+
+print(call_summary("test", "arg1", "arg2", "arg3", extra=True, data=[1,2,3]))
+
 print(statistics(1,2,3))
 print(statistics(-100, 100))
 print(statistics(77, 60, 90, 87))
+
+# Five scope questions and their output:
+
+# 1. Output is "hi"
+greeting = "hello"
+def greet():
+    greeting = "hi"
+    print(greeting)
+
+greet()
+
+# 2. Output is "hello"
+greeting = "hello"
+def greet():
+    greeting = "hi"
+    return greeting
+
+greet()
+print(greeting)
+
+# 3. Output is "hi"
+global new_greeting
+new_greeting = "hello"
+def greet():
+    new_greeting = "hi"
+    print(new_greeting)
+
+greet()
+
+# 4. Output is "hi"
+greeting = "hello"
+def greet():
+    greeting = "hi"
+
+    def inner_greet():
+        print(greeting)
+
+    inner_greet()
+
+greet()
+
+# 5. Output is "hello"
+greeting = "hello"
+def greet():
+    greeting = "hi"
+
+    def inner_greet():
+        greeting = "hello again"
+
+    inner_greet()
+
+greet()
+print(greeting)
 
 
