97822ec8161c89df4ba0f09f6a19d47918d4011b
S. Lindqvist <sanne.lindqvist@protonmail.com>
2026-09-22 14:10:12 +0200

    Use objects for students

--- lab7/part_E.py
+++ lab7/part_E.py
@@ -2,14 +2,19 @@ class Teacher:
     def __init__(self, name: str):
         self.name = name
 
+class Student:
+    def __init__(self, name: str):
+        self.name = name
+
 class Course:
     def __init__(self, name: str, teacher: Teacher):
         self.name = name
         self.teacher = teacher
-        self.students = []
+        self.students: list[Student] = []
 
-    def add_student(self, student: dict):
+    def add_student(self, student: Student):
         self.students.append(student)
+
 
 teacher = Teacher("Grace Hopper")
 course = Course("Compilers", teacher)
@@ -17,9 +22,13 @@ course = Course("Compilers", teacher)
 print("Course name:", course.name)
 print("Teacher name:", course.teacher.name)
 
-course.add_student({"name": "Scout"})
-course.add_student({"name": "Arabella"})
-course.add_student({"name": "Tamino"})
+student1 = Student("Scout")
+student2 = Student("Arabella")
+student3 = Student("Tamino")
+
+course.add_student(student1)
+course.add_student(student2)
+course.add_student(student3)
 
 for student in course.students:
-    print(student.get("name"))
+    print(student.name)
