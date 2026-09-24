4f989f2a46088f3ca2ca8722f1bed0217f444b3c
S. Lindqvist <sanne.lindqvist@protonmail.com>
2026-09-24 12:02:40 +0200

    Finished part D

---
+++ python_fundamentals/lab9/part_D.py
@@ -0,0 +1,17 @@
+class User:
+    def __init__(self, username):
+        self.username = username
+
+class AdminUser(User):
+    def __init__(self, username):
+        super().__init__(username)
+        self.is_admin = True
+
+admin  = AdminUser("admin")
+
+print(isinstance(admin, AdminUser))
+print(isinstance(admin, User))
+print(isinstance(admin, str))
+
+# AdminUser is also considered an instance of User since AdminUser inherits from User.
+# Or in other words, AdminUser is a subclass of User.
