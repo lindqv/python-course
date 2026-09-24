b6b0a3d3269fb218f09d9490bd3ec62124d76b38
S. Lindqvist <sanne.lindqvist@protonmail.com>
2026-09-24 13:34:57 +0200

    Finished part E

---
+++ python_fundamentals/lab9/part_E.py
@@ -0,0 +1,16 @@
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        return f"Name: {self.name}, price: {self.price}"
+
+
+cakes = [Product("Chocolate cake", 30), Product("Strawberry cake", 35), Product("Blueberry cake", 35)]
+
+for cake in cakes:
+    print(cake)
+
+readable_cake = str(cakes[0])
+print(type(readable_cake))
