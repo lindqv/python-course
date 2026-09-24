84b1a5b9f0adc2f3393d75d10aafa99a77caa1a4
S. Lindqvist <sanne.lindqvist@protonmail.com>
2026-09-24 16:09:44 +0200

    Add example of composition

--- python_fundamentals/lab9/part_H.py
+++ python_fundamentals/lab9/part_H.py
@@ -16,9 +16,22 @@ class TextExporter(Exporter):
     def __str__(self):
         return "Text exporter"
 
+class Title:
+    def __init__(self, title: str):
+        self.title = title
+
+    def __str__(self):
+        return self.title.strip().title()
+
 class SummaryExporter(Exporter):
+    def __init__(self, title: Title):
+        self.title = title
+
     def export(self, data):
-        return f"Summary {str(data)}"
+        return (
+            f"{self.title}\n"
+            f"Summary {str(data)}"
+        )
 
     def __str__(self):
         return "Summary exporter"
@@ -30,11 +43,11 @@ class UnrelatedExporter:
     def __str__(self):
         return "Unrelated exporter"
 
-
 data = "Testing data"
+title = Title("Summary title")
 console = ConsoleExporter()
 text = TextExporter()
-summary = SummaryExporter()
+summary = SummaryExporter(title)
 unrelated = UnrelatedExporter()
 exporters = [console, text, summary, unrelated]
 
@@ -47,3 +60,5 @@ print(isinstance(summary, Exporter))
 print(isinstance(unrelated, Exporter))
 print(isinstance(console, TextExporter))
 
+# The example of compsition in this program is the Title class that is used in the SummaryExporter.
+# SummaryExporter has-a Title
