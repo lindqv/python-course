ccbc18539b6964d12f563cfb8413d670a02ac6fb
S. Lindqvist <sanne.lindqvist@protonmail.com>
2026-09-24 11:18:35 +0200

    Finished part B

---
+++ python_fundamentals/lab9/part_B.py
@@ -0,0 +1,19 @@
+class Document:
+    def __init__(self, title):
+        self.title = title
+
+    def describe(self):
+        return "This is a document"
+
+class PDFDocument(Document):
+    def describe(self):
+        return "This document is a PDF"
+
+class TextDocument(Document):
+    def describe(self):
+        return "This document is a text document"
+
+documents = [PDFDocument("pdf1"), TextDocument("text1"), PDFDocument("pdf2"), TextDocument("text2")]
+
+for document in documents:
+    print(f"{document.title}, {document.describe()}")
