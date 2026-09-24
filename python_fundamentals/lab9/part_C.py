12a3a8d19bcd7af4c16fb77effa51e01b15291d6
S. Lindqvist <sanne.lindqvist@protonmail.com>
2026-09-24 11:56:44 +0200

    Finished part C

---
+++ python_fundamentals/lab9/part_C.py
@@ -0,0 +1,23 @@
+class Printer:
+    def __init__(self, status):
+        self.status = status
+
+    def display_status(self):
+        return "Printer status " + self.status
+
+class Screen:
+    def __init__(self, status):
+        self.status = status
+
+    def display_status(self):
+        return "Screen status " + self.status
+
+objects = [Printer("on"), Screen("off")]
+
+for object in objects:
+    print(object.display_status())
+
+# This works because each object in the list has the display_status() method.
+# With duck typing the presence of a method (or attribute) is the important part, 
+# rather than which type the objects have.
+# The duck test: "If it walks like a duck and quacks like a duck, then it must be a duck."
